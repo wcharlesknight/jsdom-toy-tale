1c658c6082f55a55446ed5884ace0e157bd28dc8
Charlie Knight <williamchknight@gmail.com>
2021-01-15 15:54:32 -0800

    worked w/ JC, Mitch and Dustin

--- src/index.js
+++ src/index.js
@@ -13,3 +13,97 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+
+
+//load it
+addAllToys()
+addEventListeners() 
+allToys = []
+///get all toys (fetch)
+
+//send new toy to server 
+function fetchToys(toy) {
+  //configuration
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(toy)
+  }
+  //fetch
+  fetch('http://localhost:3000/toys', configObj)
+    .then(resp => resp.json())
+    .then(toy => addAllToys(toy))
+    .catch(() => {
+      alert("Hey, youre fine")
+    })
+  }
+
+function addAllToys() {
+  return fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(toys => toys.forEach(toy => addToyInfo(toy)))
+}
+//add toys to page
+function addToyInfo(toy) {
+  const toyCollection = document.querySelector('#toy-collection')
+  //create card 
+  let div = document.createElement('div')
+  let h2 = document.createElement('h2')
+  let img = document.createElement('img')
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  div.className = 'card'
+  img.className = 'toy-avatar'
+  btn.className = 'like-btn'
+  p.id = toy.name
+  //add event listener
+  btn.addEventListener('click',addLikes)
+  //pass toy to those elements
+  h2.textContent = toy.name
+  img.src = toy.image
+  p.textContent = `Likes: ${toy.likes}`
+  btn.textContent = "Like"
+  btn.id = toy.id
+  allToys.push(toy)
+  //append children
+  toyCollection.appendChild(div)
+
+  div.appendChild(h2)
+  div.appendChild(img)
+  div.appendChild(p)
+  div.appendChild(btn)  
+
+}
+//handlesubmit
+function handleSubmit(e) {
+  e.preventDefault()
+  let toy = {
+    "name": e.target.name.value,
+    "image": e.target.image.value,
+    "likes": 0
+  }
+  fetchToys(toy)
+}
+
+function updateLikes(likes, p) {
+  p.innerHTML = `Likes: ${likes}`
+}
+//add Likes to Toys 
+function addLikes(e) {
+  let selection = parseInt(e.target.id,10)
+  let toy = allToys.find(toy => toy.id === selection)
+  let p = document.getElementById(`${toy.name}`)
+  likes = toy.likes + 1
+  p.innerHTML = ` `
+  updateLikes(likes, p)
+}
+
+//listeners
+function addEventListeners() {
+  let form = document.querySelector(".add-toy-form")
+  form.addEventListener('submit',handleSubmit)
+}
